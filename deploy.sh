aef926ab4bae8c4c3bcecdc8192cef835a767168
josh <josh@tesseractmobile.com>
2024-08-01 18:01:35 -0500

    Clearing docs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 set -e
 ./increment_version.sh
-flutter build web
-# This is where gihub pages will be deployed
+flutter build web --release
+# This is where github pages will be deployed
+rm -Rf docs/*
 mv build/web/* docs/
+git commit -am "Deploy to GitHub pages"
+git push
 flutter build appbundle
